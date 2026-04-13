4f35fbef5c8bf929852999c022f976009e802605
lucas <lucas.olier2003@icloud.com>
2026-04-13 09:26:33 +0200

    feat(src): initialise Dashboard (imports + style)

--- GuardDrive/src/pages/Dashboard.tsx
+++ GuardDrive/src/pages/Dashboard.tsx
@@ -1,5 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import { mockRecentAlerts, mockVehicle } from '../mock/vehicle.mock';
+import '../styles/dashboard.css';
+
 function Dashboard() {
-	return <h1>Dashboard</h1>;
+	const navigate = useNavigate();
+	const vehicle = mockVehicle;
+	const recentAlerts = mockRecentAlerts;
+
+	return <h1 onClick={() => navigate('/alertes')}>{vehicle.name} ({recentAlerts.length})</h1>;
 }
 
 export default Dashboard;
